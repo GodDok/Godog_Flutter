2db5abd5e6ad20f5d24763d897b015783cccaca4
changs97 <kin2027@gmail.com>
2024-05-02 20:17:12 +0900

    :lipstick: 로그인 화면 UI 추가

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,7 +1,26 @@
+
 import 'package:flutter/material.dart';
 
-class LoginScreen extends StatelessWidget {
+import 'join_step1_screen.dart';
+
+class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  String email = '';
+  String password = '';
+
+  Color getButtonColor() {
+    if (email.isNotEmpty && password.isNotEmpty) {
+      return Colors.blue;
+    } else {
+      return Colors.grey;
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,7 +29,96 @@ class LoginScreen extends StatelessWidget {
       body: Padding(
         padding: const EdgeInsets.all(25),
         child: Column(
-          children: [],
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const SizedBox(
+              height: 100,
+            ),
+            Row(
+              children: [
+                Image.asset(
+                  'assets/images/main_logo.png',
+                  width: 65,
+                  height: 65,
+                ),
+                const Text('창신',
+                    style: TextStyle(
+                      fontSize: 40,
+                      fontWeight: FontWeight.w800,
+                    )),
+              ],
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            TextFormField(
+              decoration: const InputDecoration(
+                labelText: '이메일',
+                border: OutlineInputBorder(),
+              ),
+              onChanged: (value) {
+                setState(() {
+                  email = value;
+                });
+              },
+            ),
+            const SizedBox(height: 20.0),
+            TextFormField(
+              decoration: const InputDecoration(
+                labelText: '비밀번호',
+                border: OutlineInputBorder(),
+              ),
+              obscureText: true,
+              onChanged: (value) {
+                setState(() {
+                  password = value;
+                });
+              },
+            ),
+            const SizedBox(height: 20.0),
+            SizedBox(
+              width: double.infinity,
+              height: 60,
+              child: ElevatedButton(
+                style: TextButton.styleFrom(
+                    shape: const RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(10),
+                      ),
+                    ),
+                    backgroundColor: getButtonColor()),
+                onPressed: () {
+                  // 로그인 버튼 눌렀을 때 실행할 동작
+                },
+                child: const Text(
+                  '로그인',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Padding(
+              padding: EdgeInsets.only(left: 10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  GestureDetector(
+                    child: Text('회원가입'),
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const JoinStep1Screen(),
+                            fullscreenDialog: true),
+                      );
+                    },
+                  )
+                ],
+              ),
+            )
+          ],
         ),
       ),
     );
