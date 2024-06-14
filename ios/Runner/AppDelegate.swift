d7346d6558d0e6d934458541525021f3528a7aca
Luslec <quandiyin@gmail.com>
2024-06-14 15:09:41 +0900

    googleMap 설정

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import GoogleMaps
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,6 +8,8 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+        // TODO: Add your Google Maps API key
+    GMSServices.provideAPIKey("AIzaSyAQISeVi0nbI6FEv8n5ZMRh-pdd_A3vT5U")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(home: StartScreen());
+    return const MaterialApp(home: MainPage());
   }
 }
 
