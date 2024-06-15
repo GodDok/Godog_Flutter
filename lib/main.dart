2e6367476b1888d60b1458206068cae1c77a8171
changs97 <kin2027@gmail.com>
2024-06-15 20:46:22 +0900

    배경 클릭 시 키보드 내리기

--- lib/main.dart
+++ lib/main.dart
@@ -13,9 +13,14 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: StartScreen(),
+    return GestureDetector(
+      onTap: () {
+        FocusManager.instance.primaryFocus?.unfocus();
+      },
+      child: const MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: StartScreen(),
+      ),
     );
   }
 }
